1ac962fdfee3b0b7acf6fe19d58ed24c410e0d9a
Yono1112 <yuma.ohno.sss@gmail.com>
2023-11-01 03:04:46 +0900

    00/00 change from ft_touppter to std::touppter

--- cpp_module_00/ex00/Megaphone.cpp
+++ cpp_module_00/ex00/Megaphone.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
-
-char	ft_toupper(char c)
-{
-	if ('a' <= c && c <= 'z')
-		c -= 32;
-	return (c);
-}
+#include <cstring>
+#include <cctype>
 
 int	main(int argc, char **argv) {
 	if (argc < 2)
@@ -13,8 +8,8 @@ int	main(int argc, char **argv) {
 	else
 	{
 		for(int i = 1; i < argc; i++) {
-			for(size_t j = 0; j < std::strlen(argv[i]); j++) {
-				std::cout << ft_toupper(argv[i][j]);
+			for(size_t j = 0; j < strlen(argv[i]); j++) {
+				std::cout << (char)std::toupper(argv[i][j]);
 			}
 		}
 		std::cout << std::endl;
