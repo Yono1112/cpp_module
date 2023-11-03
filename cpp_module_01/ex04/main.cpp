531ca310c35f3d32ca7a3e334f081d8c208e03d4
Yono1112 <yuma.ohno.sss@gmail.com>
2023-11-03 17:44:41 +0900

    01/04 change const char of arguments in ifstream and ofstream values

--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -42,23 +42,23 @@ int	main(int argc, char **argv)
 	filename = argv[1];
 	s1 = argv[2];
 	s2 = argv[3];
-	std::ifstream	ifs(filename);
+	std::ifstream	ifs(filename.c_str());
 	if (ifs.fail())
 	{
 		std::cerr << "cannnot open file" << std::endl;
 		return (1);
 	}
-	std::ofstream	ofs(filename.append(".replace"));
+	std::ofstream	ofs(filename.append(".replace").c_str());
 	if (getline(ifs, input_line))
 	{
 		current_line = input_line;
 		while (getline(ifs, input_line))
 		{
-			std::cout << "current_line '" << current_line << "'" << std::endl;
+			// std::cout << "current_line '" << current_line << "'" << std::endl;
 			ofs << replace_str(current_line, s1, s2) << std::endl;
 			current_line = input_line;
 		}
-		std::cout << "current_line '" << current_line << "'" << std::endl;
+		// std::cout << "current_line '" << current_line << "'" << std::endl;
 		ofs << replace_str(current_line, s1, s2);
 	}
 	return (0);
