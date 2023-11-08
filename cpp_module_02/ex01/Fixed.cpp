c21828d143ee6fb38d48efbd3965877e1e61bd44
Yono1112 <yuma.ohno.sss@gmail.com>
2023-11-08 15:59:26 +0900

    02/01 create 4 public member functions

--- cpp_module_02/ex01/Fixed.cpp
+++ cpp_module_02/ex01/Fixed.cpp
@@ -12,9 +12,47 @@ Fixed::Fixed(const Fixed& fix)
 	operator=(fix);
 }
 
+Fixed::Fixed(const int& num)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_fixed_point_num = num << _bits;
+	// std::cout << "_fixed_point_num: " << this->_fixed_point_num << std::endl;
+}
+
+Fixed::Fixed(float num)
+{
+	std::cout << "Float constructor called" << std::endl;
+	float	fixed_point_num;
+	int		int_num;
+	float	fraction_num;
+
+	fixed_point_num = (num * (1 << _bits));
+	// std::cout << "fixed_point_num: " << fixed_point_num << std::endl;
+	int_num = std::floor(fixed_point_num);
+	// std::cout << "int_num: " << int_num << std::endl;
+	fraction_num = fixed_point_num - int_num;
+	// std::cout << "fraction_num: " << fraction_num << std::endl;
+	if (fraction_num > 0.5)
+		this->_fixed_point_num = static_cast<int>(fixed_point_num + 1);
+	else
+		this->_fixed_point_num = static_cast<int>(fixed_point_num);
+	// std::cout << "_fixed_point_num: " << this->_fixed_point_num << std::endl;
+}
+
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
+}
+
+int		Fixed::toInt(void) const
+{
+	return (this->_fixed_point_num >> _bits);
+}
+
+float		Fixed::toFloat(void) const
+{
+	// std::cout << "toFloat: _fixed_point_num: " << this->_fixed_point_num << std::endl;
+	return (static_cast<float>(this->_fixed_point_num) / static_cast<float>(1 << _bits));
 }
 
 Fixed&	Fixed::operator=(const Fixed& fix)
@@ -25,14 +63,20 @@ Fixed&	Fixed::operator=(const Fixed& fix)
 	return (*this);
 }
 
+std::ostream&	operator<<(std::ostream& stream, const Fixed& fix)
+{
+	stream << fix.toFloat();
+	return (stream);
+}
+
 int	Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
+	// std::cout << "getRawBits member function called" << std::endl;
 	return (this->_fixed_point_num);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << std::endl;
+	// std::cout << "setRawBits member function called" << std::endl;
 	this->_fixed_point_num = raw;
 }
