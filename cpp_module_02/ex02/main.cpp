b8b0f0d0de496ade04da9e1371edf69b370dc0ad
Yono1112 <yuma.ohno.sss@gmail.com>
2023-11-09 19:23:04 +0900

    refactor test print in main

--- cpp_module_02/ex02/main.cpp
+++ cpp_module_02/ex02/main.cpp
@@ -1,48 +1,104 @@
 #include "Fixed.hpp"
 
-int	main(void)
+void	print_subject(void)
 {
-	Fixed	a;
-	Fixed	b(10);
-	Fixed	c(42.42f);
-	Fixed	d(b);
+	Fixed			a;
+	Fixed const 	b(Fixed(5.05f) * Fixed(2));
 
-	a = Fixed(1234.4321f);
+	std::cout << "run print_subject()" << std::endl;
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 
+	std::cout << b << std::endl;
+
+	std::cout << Fixed::max(a, b) << std::endl;
+	std::cout << "===========================================" << std::endl;
+}
+
+void	print_comparison(void)
+{
+	Fixed	a(42.42f);
+	Fixed	b(-42.42f);
+
+	std::cout << "run print_comparison()" << std::endl;
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	std::cout << "a > b " << (a > b) << std::endl;
 	std::cout << "a < b " <<(a < b) << std::endl;
 	std::cout << "a >= b " <<(a >= b) << std::endl;
 	std::cout << "a <= b " <<(a <= b) << std::endl;
 	std::cout << "a == b " <<(a == b) << std::endl;
 	std::cout << "a != b " <<(a != b) << std::endl;
+	std::cout << "===========================================" << std::endl;
+}
+
+void	print_arithmetic(void)
+{
+	Fixed	a(42.42f);
+	Fixed	b(10);
+
+	std::cout << "run print_arithmetic()" << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
 	std::cout << "a + b " <<(a + b) << std::endl;
 	std::cout << "a - b " <<(a - b) << std::endl;
 	std::cout << "a * b " <<(a * b) << std::endl;
 	std::cout << "a / b " <<(a / b) << std::endl;
+	std::cout << "===========================================" << std::endl;
+}
+
+void	print_increment_decrement(void)
+{
+	Fixed	a(42.42f);
+
+	std::cout << "run print_increment_decrement()" << std::endl;
+	std::cout << "a is " << a << std::endl;
 	std::cout << "++a " << ++a << std::endl;
+	std::cout << "a is " << a << std::endl;
 	std::cout << "--a " << --a << std::endl;
 	std::cout << "a is " << a << std::endl;
 	std::cout << "a++ " << a++ << std::endl;
 	std::cout << "a is " << a << std::endl;
 	std::cout << "a-- " << a-- << std::endl;
 	std::cout << "a is " << a << std::endl;
+	std::cout << "===========================================" << std::endl;
+}
+
+void	print_min_max(void)
+{
+	Fixed		a(42.42f);
+	Fixed		b(10);
+	Fixed const	first(10);
+	Fixed const	second(-10);
+
+	std::cout << "run print_min_max()" << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
 	std::cout << "min(a, b)" << std::endl;
 	std::cout << Fixed::min(a, b) << " is min" << std::endl;
 	std::cout << "max(a, b)" << std::endl;
 	std::cout << Fixed::max(a, b) << " is max" << std::endl;
-	const Fixed	first(10), second(11);
-	std::cout << "const first is " << first << ", second is "<< second << std::endl;
+	std::cout << "const first is " << first << ", const second is "<< second << std::endl;
 	std::cout << "min(first, second) " << std::endl;
 	std::cout << Fixed::min(first, second) << " is min" << std::endl;
 	std::cout << "max(first, second) " << std::endl;
 	std::cout << Fixed::max(first, second) << " is max" <<  std::endl;
+	std::cout << "min(a, second) " << std::endl;
+	std::cout << Fixed::min(a, second) << " is min" << std::endl;
+	std::cout << "max(first, b) " << std::endl;
+	std::cout << Fixed::max(first, b) << " is max" <<  std::endl;
+	std::cout << "===========================================" << std::endl;
+}
+
+int	main(void)
+{
+	print_subject();
+	print_comparison();
+	print_arithmetic();
+	print_increment_decrement();
+	print_min_max();
 	return (0);
 }
