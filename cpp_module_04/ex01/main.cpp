d5abc807d47e6daaebe451b67c15c896f8b15153
Yono1112 <yuma.ohno.sss@gmail.com>
2023-11-23 15:25:11 +0900

    04/01 cp ex00 files

---
+++ cpp_module_04/ex01/main.cpp
@@ -0,0 +1,17 @@
+#include "Animal.hpp"
+#include "Dog.hpp"
+#include "Cat.hpp"
+
+int	main(void)
+{
+	const Animal* meta = new Animal();
+	const Animal* i = new Cat();
+	const Animal* j = new Dog();
+
+	std::cout << i->getType() << " " << std::endl;
+	std::cout << j->getType() << " " << std::endl;
+	i->makeSound();
+	j->makeSound();
+	meta->makeSound();
+	return (0);
+}
