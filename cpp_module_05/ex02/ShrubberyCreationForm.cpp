5c9106f877a0b0aa69e70caee072e7bfb3d6691f
Yono1112 <yuma.ohno.sss@gmail.com>
2023-12-14 21:15:51 +0900

    05/02 output Ascii Tree

--- cpp_module_05/ex02/ShrubberyCreationForm.cpp
+++ cpp_module_05/ex02/ShrubberyCreationForm.cpp
@@ -1,4 +1,5 @@
 #include "ShrubberyCreationForm.hpp"
+#include <iomanip>
 
 ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target)
 	: AForm(target, false, 145, 137)
@@ -14,4 +15,16 @@ ShrubberyCreationForm::~ShrubberyCreationForm()
 void	ShrubberyCreationForm::execute(Bureaucrat const& executor) const
 {
 	std::cout << "ShrubberyCreationForm Hello " << executor.getName() << "!" << std::endl;
+    	int padding = 9;
+	for (int i = 1; i <= 15; i += 2) {
+		std::cout << std::setw(padding--);
+		for (int j = 0; j < i; j++) {
+		    std::cout << 'X';
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::setw(10) << "###" << std::endl;
+	std::cout << std::setw(10) << "###" << std::endl;
+	std::cout << std::setw(11) << "#####" << std::endl;
+	std::cout << std::setw(16) << "HAPPY HOLLIDAYS" << std::endl;
 }
