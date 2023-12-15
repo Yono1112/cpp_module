6913830c9a1398917f9457080bf53a45d7d8cf67
Yuma Ohno <yuohno@c1r27s10.42tokyo.jp>
2023-12-16 08:53:50 +0900

    05/02 create file and write ascii message to the file in ShrubberyCreationForm

--- cpp_module_05/ex02/ShrubberyCreationForm.cpp
+++ cpp_module_05/ex02/ShrubberyCreationForm.cpp
@@ -1,5 +1,6 @@
 #include "ShrubberyCreationForm.hpp"
 #include <iomanip>
+#include <fstream>
 
 ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target)
 	: AForm(target, false, 145, 137)
@@ -12,19 +13,27 @@ ShrubberyCreationForm::~ShrubberyCreationForm()
 	std::cout << "ShrubberyCreationForm Destructor Called" << std::endl;
 }
 
+void	write_ascii_tree(std::ofstream& ofs)
+{
+   	int padding = 9;
+	for (int i = 1; i <= 15; i += 2) {
+		ofs << std::setw(padding--);
+		for (int j = 0; j < i; j++) {
+		    ofs << "X";
+		}
+		ofs << std::endl;
+	}
+	ofs << std::setw(10) << "###" << std::endl;
+	ofs << std::setw(10) << "###" << std::endl;
+	ofs << std::setw(11) << "#####" << std::endl;
+	ofs << std::setw(16) << "HAPPY HOLLIDAYS" << std::endl;
+}
+
 void	ShrubberyCreationForm::execute(Bureaucrat const& executor) const
 {
 	std::cout << "ShrubberyCreationForm Hello " << executor.getName() << "!" << std::endl;
-    	int padding = 9;
-	for (int i = 1; i <= 15; i += 2) {
-		std::cout << std::setw(padding--);
-		for (int j = 0; j < i; j++) {
-		    std::cout << 'X';
-		}
-		std::cout << std::endl;
-	}
-	std::cout << std::setw(10) << "###" << std::endl;
-	std::cout << std::setw(10) << "###" << std::endl;
-	std::cout << std::setw(11) << "#####" << std::endl;
-	std::cout << std::setw(16) << "HAPPY HOLLIDAYS" << std::endl;
+	std::ofstream	ofs(executor.getName() + "_shrubbery");
+	if (ofs.fail())
+		throw "cannot open file";
+	write_ascii_tree(ofs);
 }
