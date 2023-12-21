04b36becbcbab7625fefb262c8483057e22a0c78
Yono1112 <yuma.ohno.sss@gmail.com>
2023-12-21 14:20:34 +0900

    06/00 create checkCharLiteral()

--- cpp_module_06/ex00/ScalarConverter.cpp
+++ cpp_module_06/ex00/ScalarConverter.cpp
@@ -1,13 +1,45 @@
 #include "ScalarConverter.hpp"
+// static bool	checkCharLiteral(const std::string& str);
 
 void	ScalarConverter::convert(const std::string& str) {
 	std::cout << "str: " << str << std::endl;
-	int literal_type = classifyLiteral(str);
-	// switch (literal_type) {
-
-	// }
+	int literal_type = detectLiteral(str);
+	switch (literal_type) {
+		case CHAR_LITERAL:
+			std::cout << "char literal" << std::endl;
+			break;
+		case INT_LITERAL:
+			std::cout << "int literal" << std::endl;
+			break;
+		case FLOAT_LITERAL:
+			std::cout << "float literal" << std::endl;
+			break;
+		case DOUBLE_LITERAL:
+			std::cout << "double literal" << std::endl;
+			break;
+		case PSEUDO_LITERAL:
+			std::cout << "pseudo literal" << std::endl;
+			break;
+		default:
+			std::cout << "undefined literal" << std::endl;
+	}
 }
 
-int	ScalarConverter::classifyLiteral(const std::string& str) {
+bool	ScalarConverter::checkCharLiteral(const std::string& str) {
+	return (str.length() == 1 && std::isprint(str[0]) && !std::isdigit(str[0]));
+}
 
+int	ScalarConverter::detectLiteral(const std::string& str) {
+	if (ScalarConverter::checkCharLiteral(str))
+		return (CHAR_LITERAL);
+	// else if (checkDoubleLiteral(str))
+	// 	return (DOUBLE_LITERAL);
+	// else if (checkFloatLiteral(str))
+	// 	return (DOUBLE_LITERAL);
+	// else if (checkIntLiteral(str))
+	// 	return (DOUBLE_LITERAL);
+	// else if (checkPreudoLiteral(str))
+	// 	return (DOUBLE_LITERAL);
+	else
+		return (-1);
 }
--- cpp_module_06/ex00/ScalarConverter.hpp
+++ cpp_module_06/ex00/ScalarConverter.hpp
@@ -5,13 +5,24 @@
 #include <iostream>
 #include <cstring>
 
+#define CHAR_LITERAL 0
+#define INT_LITERAL 1
+#define FLOAT_LITERAL 2
+#define DOUBLE_LITERAL 3
+#define PSEUDO_LITERAL 4
+
 class ScalarConverter {
 	public:
 		static void	convert(const std::string& str);
 	private:
 		ScalarConverter();
 		~ScalarConverter();
-		int	classifyLiteral(const std::string& str);
+		static int	detectLiteral(const std::string& str);
+		static bool	checkCharLiteral(const std::string& str);
+		// bool	checkDoubleLiteral(const std::string& str);
+		// bool	checkFloatLiteral(const std::string& str);
+		// bool	checkIntLiteral(const std::string& str);
+		// bool	checkPreudoLiteral(const std::string& str);
 };
 
 #endif
