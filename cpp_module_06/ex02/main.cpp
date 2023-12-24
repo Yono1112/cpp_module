13136231021fbbb14d1fcc965587b9c40c1c8c95
Yono1112 <yuma.ohno.sss@gmail.com>
2023-12-24 22:24:39 +0900

    06/02 create geterate()

--- cpp_module_06/ex02/main.cpp
+++ cpp_module_06/ex02/main.cpp
@@ -3,19 +3,34 @@
 #include "B.hpp"
 #include "C.hpp"
 #include <iostream>
+#include <cstdlib>
 
 Base*	generate(void) {
-
+	std::srand(time(NULL));
+	int index = rand() % 3;
+	if (index == 0) {
+		std::cout << "A class" << std::endl;
+		return (new A());
+	} else if (index == 1) {
+		std::cout << "B class" << std::endl;
+		return (new B());
+	} else {
+		std::cout << "C class" << std::endl;
+		return (new C());
+	}
 }
 
-void	identify(Base* p) {
-
-}
-
-void	identify(Base& p) {
-
-}
+// void	identify(Base* p) {
+// 
+// }
+// 
+// void	identify(Base& p) {
+// 
+// }
 
 int main() {
+	Base* ptr = generate();
+	std::cout << ptr << std::endl;
+	delete ptr;
 	return (0);
 }
