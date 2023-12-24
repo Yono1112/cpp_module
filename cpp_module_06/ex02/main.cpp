9e867b014258cc8cd0b5772ebe21a77bc075a27a
Yono1112 <yuma.ohno.sss@gmail.com>
2023-12-24 22:14:18 +0900

    remove some files and create Base, A, B and C class

---
+++ cpp_module_06/ex02/main.cpp
@@ -0,0 +1,21 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+
+Base*	generate(void) {
+
+}
+
+void	identify(Base* p) {
+
+}
+
+void	identify(Base& p) {
+
+}
+
+int main() {
+	return (0);
+}
