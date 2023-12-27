7bfd9faf8342eddf6dc8c0b5c9de16e6ae94b865
Yuma Ohno <yuohno@c1r27s8.42tokyo.jp>
2023-12-27 21:25:17 +0900

    05/03 change message of makeForm() according to subject

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -35,6 +35,6 @@ AForm*	Intern::makeForm(const std::string& name, const std::string& target)
 		default:
 			throw "the form name passed as parameter doesn’t exist";
 	}
-	std::cout << "Intern create " << form_list[i] << "." << std::endl;
+	std::cout << "Intern creates " << form_list[i] << std::endl;
 	return (obj);
 }
