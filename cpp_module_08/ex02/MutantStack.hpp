58e0d94be9ae5bebf956ad3a12fb183a32f77508
Yono1112 <yuma.ohno.sss@gmail.com>
2023-12-30 20:06:10 +0900

    08/02 create member function in std::stack

--- cpp_module_08/ex02/MutantStack.hpp
+++ cpp_module_08/ex02/MutantStack.hpp
@@ -11,11 +11,25 @@ class MutantStack {
 		MutantStack(const MutantStack& other);
 		~MutantStack();
 		MutantStack& operator=(const MutantStack& other);
-		// void	push(const T& value);
-		// void	pop();
-		// T&	top();
-		// bool	empty();
-		// size_t	size();
+		void	push(const T& value) {
+			_container.push_back(value);
+		};
+		void	pop() {
+			if (_container.empty())
+				return ;
+			_container.pop_back();
+		};
+		T&	top() {
+			if (_container.empty())
+				throw std::out_of_range("stack is empty");
+			return (_container.back());
+		};
+		bool	empty() {
+			return (_container.empty());
+		};
+		size_t	size() {
+			return (_container.size());
+		};
 	private:
 		Container _container;
 };
--- cpp_module_08/ex02/main.cpp
+++ cpp_module_08/ex02/main.cpp
@@ -1,10 +1,23 @@
 #include "MutantStack.hpp"
 
 int	main() {
-	MutantStack<int, std::deque<int> >	 mstack1;
-	MutantStack<int>	 mstack2(mstack1);
-	MutantStack<int>	 mstack3;
+	try {
+		MutantStack<int>	 mstack1;
+		// MutantStack<int, std::vector<int> >	 mstack2(mstack1);
 
-	mstack3 = mstack1;
+		std::cout << "mstack size: " << mstack1.size() << std::endl;
+		mstack1.push(1);
+		mstack1.pop();
+		std::cout << "mtack empty: " << std::boolalpha << mstack1.empty() << std::endl;
+		for (int i = 0; i < 10; i++) {
+			std::cout << "push: " << i << std::endl;
+			mstack1.push(i);
+			std::cout << "mstack top: " << mstack1.top() << std::endl;
+		}
+		std::cout << "mtack empty: " << std::boolalpha << mstack1.empty() << std::endl;
+		std::cout << "mstack size: " << mstack1.size() << std::endl;
+	} catch (const std::exception& e) {
+		std::cout << "catch std::exception: " << e.what() << std::endl;
+	}
 	return (0);
 }
